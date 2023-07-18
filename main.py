9a77a5e0cf0533cc9e7584c1dd6f84710b470644
itsc222 <ischneid87@gmail.com>
2023-07-17 19:40:39 -0700

    generated table of 50 artists

--- main.py
+++ main.py
@@ -4,19 +4,33 @@ import polars as pl
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-limit = 10
+data_full = {"name": [], "popularity": [], "followers": []} 
 
-print(list(range(limit)))
+full_table = pl.DataFrame(data_full, schema={"name": str, "popularity": pl.Int64, "followers": pl.Int64})
+
+print(full_table)
+
+limit = 50
+index = (list(range(limit)))
 
 results = spotify.search(q='genre:k-pop', limit = limit, type='artist')
 
-followers = results['artists']['items'][0]['followers']['total']
-name = results['artists']['items'][0]['name']
-popularity = results['artists']['items'][0]['popularity']
+for i in index:
+    followers = results['artists']['items'][i]['followers']['total']
+    name = results['artists']['items'][i]['name']
+    popularity = results['artists']['items'][i]['popularity']
 
-print(tuple(name, popularity, followers))
+    data = {"name": name, "popularity": popularity, "followers": followers} 
+    df = pl.DataFrame(data)
 
+    full_table.extend(df)
 
+    
+
+print(full_table)
+
+path = "spotify_k_pop_popularity.csv"
+full_table.write_csv(path, separator= ",")
 
 #artists = results['items']
 #while results['next']:
